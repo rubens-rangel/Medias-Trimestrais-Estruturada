95fbf7e2c6ab605c588dc4aea2739d642a34714a
Rubens Rangel <rubens_rangel@hotmail.com>
2021-08-21 18:04:16 -0300

    Medias trimestrais Estruturada para a materia de Projeto Integrador I

--- src/MediasTriEstru.java
+++ src/MediasTriEstru.java
@@ -35,7 +35,6 @@ public class MediasTriEstru {
             media[i]=nota1t[i]+nota2t[i]+nota3t[i];
             System.out.printf("Media: %.1f\n",media[i]);
 
-
             if (media[i] >= 60.00 ){ status[i] = "Aprovado"; }
             else if (media[i] < 50.00){ status[i] = "Reprovado";}
             else if (media[i] >= 50.00 && media[i] <= 59.99){ status[i] = "Fara a Avaliacao Final"; }
